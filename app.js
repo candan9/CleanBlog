77d506fc51fb2f2e8e95d4dc975127f9ac580cbd
emre candan <wastorist@gmail.com>
2021-10-12 18:23:50 +0300

    2.bolum odev 8 gerceklendi.

--- app.js
+++ app.js
@@ -1,13 +1,27 @@
 const express = require('express');
+const ejs = require('ejs');
+const path = require('path');
 const app = express();
-app.get('/',(req,res)=>{
-    const blog ={
-        id:1,
-        title :"Blog title",
-        description:"Blog description"
-    }
-    res.send(blog)
-})
+
+// TEMPLATE ENGINE
+app.set('view engine', 'ejs');
+
+//MIDDLEWARE
+app.use(express.static('public'));
+
+//ROUT
+app.get('/', (req, res) => {
+  res.render('index');
+});
+app.get('/about', (req, res) => {
+  res.render('about');
+});
+app.get('/add', (req, res) => {
+  res.render('add_post');
+});
+app.get('/post', (req, res) => {
+  res.render('post');
+});
 const port = 3000;
 app.listen(port, () => {
   console.log(`Sunucu ${port} portunda başlatıldı.`);
