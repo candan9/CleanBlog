1d47d83b6fc4fe0db1f69a63e01db6c0bb59b946
Emre Candan <49526501+candan9@users.noreply.github.com>
2021-12-14 14:36:36 +0300

    Update postController.js
    Project editing

--- controllers/postController.js
+++ controllers/postController.js
@@ -10,7 +10,7 @@ exports.getAllPosts = async (req, res) => {
 exports.getPost = async (req, res) => {
   const post = await Post.findById(req.params.id);
   res.render('post', {
-    post,
+    post, 
   });
 };
 
